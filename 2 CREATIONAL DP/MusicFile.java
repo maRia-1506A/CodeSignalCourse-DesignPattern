0948258148e2563d229f481067001f32cc8af013
Zannatul Maria <zannatulfarzana363@gmail.com>
2026-03-23 12:00:00 +0600

    abstract

---
+++ 2 CREATIONAL DP/MusicFile.java
@@ -0,0 +1,43 @@
+// TODO: Define the abstract class MusicFile with an abstract play method
+// TODO: Declare the abstract method play()
+
+// TODO: Define MP3File class which extends MusicFile
+// TODO: Implement the play method to print "Playing MP3 File."
+
+// TODO: Define WAVFile class which extends MusicFile
+// TODO: Implement the play method to print "Playing WAV File."
+
+// TODO: Define FLACFile class which extends MusicFile
+// TODO: Implement the play method to print "Playing FLAC File."
+
+// TODO: Define the MusicFileAbstractFactory interface with a createMusicFile method
+// TODO: Declare the abstract method createMusicFile()
+
+// TODO: Define MP3FileFactory class which implements MusicFileAbstractFactory
+// TODO: Implement the createMusicFile method to return a new MP3File instance
+
+// TODO: Define WAVFileFactory class which implements MusicFileAbstractFactory
+// TODO: Implement the createMusicFile method to return a new WAVFile instance
+
+// TODO: Define FLACFileFactory class which implements MusicFileAbstractFactory
+// TODO: Implement the createMusicFile method to return a new FLACFile instance
+
+// TODO: Define MusicFileFactory class with a static createMusicFile method
+// TODO: Implement the createMusicFile method to utilize a factory to create a music file
+
+// create a main class 
+// Create an MP3 File
+// TODO: Create an MP3File using MusicFileFactory and the respective factory
+// TODO: Call play method on the created MP3File
+
+// Create a WAV File
+// TODO: Create a WAVFile using MusicFileFactory and the respective factory
+// TODO: Call play method on the created WAVFile
+
+// Create a FLAC File
+// TODO: Create a FLACFile using MusicFileFactory and the respective factory
+// TODO: Call play method on the created FLACFile
+
+public class MusicFile {
+
+}
