3f14325018de043c47e84b5e9d2b91e1273eb84b
Zannatul Maria <zannatulfarzana363@gmail.com>
2026-03-25 12:00:00 +0600

    commit

--- 2 CREATIONAL DP/MusicFile.java
+++ 2 CREATIONAL DP/MusicFile.java
@@ -1,4 +1,4 @@
-// TODO: Define the abstract class MusicFile with an abstract play method
+// TODO: Define the ABSTRACT class MusicFile with an abstract play method
 // TODO: Declare the abstract method play()
 
 // TODO: Define MP3File class which extends MusicFile
@@ -38,9 +38,58 @@
 // TODO: Create a FLACFile using MusicFileFactory and the respective factory
 // TODO: Call play method on the created FLACFile
 
-public class MusicFile {
+public abstract class MusicFile {
+    public abstract void play();
+
     public static void main(String[] args) {
         
     }
+}
 
+class MP3File extends MusicFile {
+    public void play() {
+        System.out.println("Playing MP3 File.");
+    }
+}
+
+class WAVFile extends MusicFile {
+    public void play() {
+        System.out.println("Playing WAV File.");
+    }
+}
+
+class FLACFile extends MusicFile {
+    public void play() {
+        System.out.println("Playing FLAC File.");
+    }
+}
+
+// interface 
+interface MusicFileAbstractFactory {
+    MusicFile createMusicFile();
+}
+
+class MP3FileFactory implements MusicFileAbstractFactory{
+    public MusicFile createMusicFile(){
+        return new MP3File();
+    }
+}
+
+class WAVFileFactory implements MusicFileAbstractFactory{
+    public MusicFile createMusicFile(){
+        return new WAVFile();
+    }
+}
+
+class FLACFileFactory implements MusicFileAbstractFactory{
+    public MusicFile createMusicFile(){
+        return new FLACFile();
+    }
+}
+
+// main factory class 
+class MusicFileFactory {
+    public static MusicFile createMusicFile(MusicFileAbstractFactory factory){
+        return factory.createMusicFile();
+    }
 }
