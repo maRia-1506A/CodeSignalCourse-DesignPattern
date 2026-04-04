2334a1e7d5c8c48e6ac25ed37a676d47683753dc
Zannatul Maria <zannatulfarzana363@gmail.com>
2026-04-04 23:28:51 +0600

    modified

--- 3 STRUCTURAL DP/Beverage.java
+++ 3 STRUCTURAL DP/Beverage.java
@@ -1,14 +1,18 @@
 // DECORATOR DP
 
-// TODO: Define the Beverage interface with a method getDescription returning String// TODO: Implement the Beverage interface in BasicCoffee class
-// TODO: Define the getDescription method to return "Black Coffee"// TODO: Implement the Beverage interface in BeverageDecorator class
+// TODO: Define the Beverage interface with a method getDescription returning String
+// TODO: Implement the Beverage interface in BasicCoffee class
+// TODO: Define the getDescription method to return "Black Coffee"
+// TODO: Implement the Beverage interface in BeverageDecorator class
 // TODO: Add a private Beverage variable named decoratedBeverage
 // TODO: Create a constructor that takes a Beverage object and assigns it to decoratedBeverage
-// TODO: Implement the getDescription method that returns decoratedBeverage.getDescription()// TODO: Extend the BeverageDecorator class in MilkDecorator
+// TODO: Implement the getDescription method that returns decoratedBeverage.getDescription() 
+// TODO: Extend the BeverageDecorator class in MilkDecorator
 // TODO: Create a constructor that takes a Beverage object and passes it to the BeverageDecorator constructor
-// TODO: Override the getDescription method to add " + Milk" to the base description.// TODO: Extend the BeverageDecorator class in SugarDecorator
+// TODO: Override the getDescription method to add " + Milk" to the base description.
+// TODO: Extend the BeverageDecorator class in SugarDecorator
 // TODO: Create a constructor that takes a Beverage object and passes it to the BeverageDecorator constructor
-// TODO: Override the getDescription method to add " + Sugar" to the base description.public class Main {
+// TODO: Override the getDescription method to add " + Sugar" to the base description.public class Main
 
 // TODO: Create instances of BasicCoffee and wrap them using MilkDecorator and SugarDecorator.
 // Print the final descriptions.
